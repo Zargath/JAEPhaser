0e75b7c48554f29d4720cb3a87672f0f6e4fdd8a
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-01-26 11:35:48 -0500

    Created first map

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -8,8 +8,9 @@ export default class MainScene extends BaseScene {
   }
 
   preload() {
-    this.load.tilemapTiledJSON('map', map);
+    this.load.tilemapTiledJSON('map', 'assets/test_map.json');
     this.load.image('mario-tiles', 'assets/sample_tileset.png');
+    this.load.image('snow-tiles', 'assets/snow_tileset.jpg');
     this.load.image('player', 'assets/player.png');
   }
 
@@ -17,8 +18,8 @@ export default class MainScene extends BaseScene {
     this.cameras.main.setViewport(120, 100, 600, 400);
 
     const map = this.make.tilemap({ key: 'map', tileWidth: 16, tileHeight: 16 });
-    const tileset = map.addTilesetImage('sample_tileset', 'mario-tiles');
-    map.createStaticLayer('first', tileset, 0, 0);
+    const tileset = map.addTilesetImage('test', 'snow-tiles');
+    map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
 
     this.player = new Player(this, 100, 100);
     this.cameras.main.startFollow(this.player, true, 0.08, 0.08);
