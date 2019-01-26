2af2826f50a59a79ff85154eb1cd48853bc3d0bb
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-01-26 00:23:41 -0500

    small changes

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -15,8 +15,7 @@ export default class MainScene extends BaseScene {
     const myMap = new Map();
     const level = myMap.getMap();
 
-    this.cameras.main.setViewport(120, 100, 800, 400);
-    this.cameras.main.setBackgroundColor('rgba(255, 0, 0, 0.5)');
+    this.cameras.main.setViewport(120, 100, 400, 400);
 
     const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
     const tiles = map.addTilesetImage('mario-tiles');
