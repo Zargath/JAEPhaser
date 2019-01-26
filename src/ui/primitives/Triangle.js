9f7e848f4d9062d1e831f0f9d962452b2a664583
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2019-01-26 00:34:07 -0500

    style changes

--- src/ui/primitives/Triangle.js
+++ src/ui/primitives/Triangle.js
@@ -1,10 +1,10 @@
 import Phaser from 'phaser';
 
 export default class Triangle extends Phaser.GameObjects.Triangle {
-    constructor(scene, x, y, x1, y1, x2, y2, x3, y3) {
-        super(scene, x, y, x1, y1, x2, y2, x3, y3);
+  constructor(scene, x, y, x1, y1, x2, y2, x3, y3) {
+    super(scene, x, y, x1, y1, x2, y2, x3, y3);
 
-        this.setStrokeStyle(2, 0xffffff, 1);
-        scene.add.existing(this);
-    }
+    this.setStrokeStyle(2, 0xffffff, 1);
+    scene.add.existing(this);
+  }
 }
