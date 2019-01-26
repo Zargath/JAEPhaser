59fd2bc290f3e1c26adc292352d5e9584558d82d
Edouard Shaar <Zarthia@gmail.com>
2019-01-25 22:20:21 -0500

    Updated Phaser to 3.15.1 and added a triangle

--- src/states/Sandbox.js
+++ src/states/Sandbox.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import BaseState from './BaseState';
 import Square from '../ui/primitives/Square';
 import JAEText from '../ui/JAEText';
+import Triangle from '../ui/primitives/Triangle';
 // import GameKeyboard from '../Inputs/GameKeyboard';
 
 export default class Sandbox extends BaseState {
@@ -12,6 +13,7 @@ export default class Sandbox extends BaseState {
 
   create() {
     this.square = new Square(10, 10, 100, this);
+    this.triangle = new Triangle(this, 100, 100, 10, 10, 15, 5, 5, 5);
     this.text = new JAEText(50, 50, 'My Text', { font: '96px Courier', fill: '#fff' }, this);
     this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     this.draw();
@@ -19,6 +21,7 @@ export default class Sandbox extends BaseState {
 
   draw() {
     this.square.draw();
+    //this.triangle.draw();
     this.text.draw();
   }
 
